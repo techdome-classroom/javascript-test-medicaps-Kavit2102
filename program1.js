e14608c96cc9cb39639d1c321dc95600e8386955
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 13:43:44 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,26 +1,18 @@
 function longestSubstring(s) {
   // Implementation of longestSubstring function
-  let maxLength = 0;
-  let start = 0;
-  const charIndexMap = {};
-
-  // Iterate through the string
-  for (let end = 0; end < s.length; end++) {
-    const currentChar = s[end];
-    // If the character is already in the map and its index is after the start of the current substring,
-    // update the start of the substring to the index after the previous occurrence of the character.
-    if (
-      charIndexMap[currentChar] !== undefined &&
-      charIndexMap[currentChar] >= start
-    ) {
-      start = charIndexMap[currentChar] + 1;
+  var k = 0;
+  var maxLength = 0;
+  for (i = 0; i < s.length; i++) {
+    for (j = k; j < i; j++) {
+      if (s[i] === s[j]) {
+        k = j + 1;
+        break;
+      }
     }
-    // Update the index of the current character in the map
-    charIndexMap[currentChar] = end;
-    // Update the maximum length
-    maxLength = Math.max(maxLength, end - start + 1);
+    if (i - k + 1 > maxLength) {
+      maxLength = i - k + 1;
+    }
   }
-
   return maxLength;
 }
 
