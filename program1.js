7f3cd2bd792aef97a7076248c78816ef4f310832
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 13:44:45 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,8 +7,6 @@ function longestSubstring(s) {
   // Iterate through the string
   for (let end = 0; end < s.length; end++) {
     const currentChar = s[end];
-    // If the character is already in the map and its index is after the start of the current substring,
-    // update the start of the substring to the index after the previous occurrence of the character.
     if (
       charIndexMap[currentChar] !== undefined &&
       charIndexMap[currentChar] >= start
