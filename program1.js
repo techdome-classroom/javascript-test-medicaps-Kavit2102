7ad7c1c9e036d9d33d330433ecddfcf8004392dd
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 13:48:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,21 +1,5 @@
 function longestSubstring(s) {
-  let maxLength = 0;
-  let start = 0;
-  const charIndexMap = {};
-
-  for (let end = 0; end < s; end++) {
-    const currentChar = s[end];
-    if (
-      charIndexMap[currentChar] !== undefined &&
-      charIndexMap[currentChar] >= start
-    ) {
-      start = charIndexMap[currentChar] + 1;
-    }
-    charIndexMap[currentChar] = end;
-    maxLength = Math.max(maxLength, end - start + 1);
-  }
-
-  return maxLength;
+  console.log("Hello");
 }
 
 module.exports = { longestSubstring };
