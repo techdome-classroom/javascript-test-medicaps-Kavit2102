54a971096214958232bba479e0d2e704c17ff5cb
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 14:06:12 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 function smallestMissingPositiveInteger(nums) {
-
   // Implement the function smallest_missing_positive_integer
-
+  nums = nums.sort(function (val1, val2) {
+    return val1 > val2 ? 1 : val1 < val2 ? -1 : 0;
+  });
+  if (nums.length === 0 || nums[nums.length - 1] <= 0) {
+    return 1;
+  } // A, B
+  for (var i = 0, k = 1; i < nums.length; ++i) {
+    // C
+    if (nums[i] > k) {
+      return k;
+    }
+    k = nums[i] < k ? k : k + 1;
+  }
+  return nums[nums.length - 1] + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
