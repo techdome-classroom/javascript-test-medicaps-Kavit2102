388e20d883a9212f14e567af5538c2526b19ee9b
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 13:37:43 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,21 @@
 function longestSubstring(s) {
   // Implementation of longestSubstring function
+  const positiveIntegers = arr.filter((num) => num > 0);
+
+  // Step 2: Construct a set to store positive integers
+  const integerSet = new Set(positiveIntegers);
+
+  // Step 3: Iterate from 1 to the maximum value in the list
+  const max = Math.max(...positiveIntegers);
+  for (let i = 1; i <= max + 1; i++) {
+    // Return the first integer that is not present in the set
+    if (!integerSet.has(i)) {
+      return i;
+    }
+  }
+
+  // Return max + 1 if all positive integers are present
+  return max + 1;
 }
 
 module.exports = { longestSubstring };
